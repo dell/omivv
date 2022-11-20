2efb18c5b49b8dcbb11049a8d624902c2df948a6
Abhishek_Gupta14 <Abhishek_Gupta14@dell.com>
2022-11-21 00:47:00 +0530

    Discover Server Device - Updates

--- Python/omevv/v1/discoverServerDevice.py
+++ Python/omevv/v1/discoverServerDevice.py
@@ -12,7 +12,7 @@ from omevv_apis_client.types import Response
 warnings.filterwarnings("ignore")
 
 class HostDiscoveryWrapper:
-    def __init__(self, base_url, omeIp, vcUsercredential, vCenterUUID, payload, jobname, jobdescription, console_entity_id, device_username, device_password):
+    def __init__(self, base_url, omeIp, vcUsercredential, vCenterUUID, payload, jobname, jobdescription, console_entity_id, device_username, device_password, use_global_creds):
         credential = vcUsercredential.username + ":" + vcUsercredential.password
         basicAuth = "Basic %s" % base64.b64encode(credential.encode('utf-8')).decode()
         self.headers = {constants.vcGuidHeader: vCenterUUID}
@@ -26,6 +26,7 @@ class HostDiscoveryWrapper:
         self.console_entity_id = console_entity_id
         self.device_username = device_username
         self.device_password = device_password
+        self.use_global_creds = use_global_creds
         self.client = AuthenticatedClient(base_url=base_url, token=None, verify_ssl=False). \
             with_headers(headers=self.headers). \
             with_timeout(constants.generalTimeOut_sec)
@@ -44,10 +45,10 @@ class HostDiscoveryWrapper:
         hostDiscoveryGroupPayload = []
         hostDiscoveryGroupPayload.append(
             {
-                "consoleEntityIDs": ['%s'%(self.console_entity_id)],
-                "userName": '%s'%(self.device_username),
+                "consoleEntityIDs": [self.console_entity_id],
+                "userName": self.device_username,
                 "passWord": self.device_password,
-                "useGlobalCredentials": False            
+                "useGlobalCredentials": self.use_global_creds            
             }
         )
 
@@ -56,11 +57,9 @@ class HostDiscoveryWrapper:
 
     def run_discovery(self):
         global retry
-        retry = 0
+        retry = 3
         headers = self.headers
         url = 'https://%s/omevv/GatewayService/v1/Consoles/%s/Hosts/Discover'%(self.omeIp, self.uuid)
-        print(url)
-        print(self.payload)
         try:
             response = requests.post(url, data=json.dumps(self.payload), headers=headers, verify=False);
             data = response.json();
@@ -68,7 +67,7 @@ class HostDiscoveryWrapper:
             if status_code == 202:
               return "Discovery job is created successfully with id "+ str(data)
             elif status_code == 400:
-                return "Error occured while creating discovery job 400: "+ str(data)
+                return "Error occured while creating discovery job : "+ str(data)
             elif status_code == 500:
                 return "Error occured while creating discovery job : "+ str(data)
             else:
@@ -99,14 +98,15 @@ if __name__ == "__main__":
     PARSER.add_argument("--console_entity_id", required=True, default=None, help="console entity id of the server device")
     PARSER.add_argument("--device_username", required=True, default=None, help="username of device")
     PARSER.add_argument("--device_password", required=True, default=None, help="password of device")
+    PARSER.add_argument("--use_global_creds", required=True, default=None, help="indicate whether to use global credentials")
 
     ARGS = PARSER.parse_args()
 
-    if ARGS.ip is not None and ARGS.vcusername is not None and ARGS.vcpassword is not None and ARGS.vcUUID is not None and ARGS.jobname is not None:
+    if ARGS.ip is not None and ARGS.vcusername is not None and ARGS.vcpassword is not None and ARGS.vcUUID is not None and ARGS.jobname is not None and ARGS.jobdescription is not None and ARGS.console_entity_id is not None and ARGS.device_username is not None and ARGS.device_password is not None and ARGS.use_global_creds is not None:
         base_url = 'https://{ip}/omevv/GatewayService/v1/'.format(ip=ARGS.ip)
         credential = Credential(username=ARGS.vcusername, password=ARGS.vcpassword)
         payload = {}
-        output=HostDiscoveryWrapper(base_url=base_url, omeIp=ARGS.ip, vcUsercredential=credential, vCenterUUID=ARGS.vcUUID, payload=payload, jobname=ARGS.jobname, jobdescription=ARGS.jobdescription, console_entity_id=ARGS.console_entity_id, device_username=ARGS.device_username, device_password=ARGS.device_password).create_payload()
-        print(HostDiscoveryWrapper(base_url=base_url, omeIp=ARGS.ip, vcUsercredential=credential, vCenterUUID=ARGS.vcUUID, payload=payload, jobname=ARGS.jobname, jobdescription=ARGS.jobdescription, console_entity_id=ARGS.console_entity_id, device_username=ARGS.device_username, device_password=ARGS.device_password).run_discovery())
+        output=HostDiscoveryWrapper(base_url=base_url, omeIp=ARGS.ip, vcUsercredential=credential, vCenterUUID=ARGS.vcUUID, payload=payload, jobname=ARGS.jobname, jobdescription=ARGS.jobdescription, console_entity_id=ARGS.console_entity_id, device_username=ARGS.device_username, device_password=ARGS.device_password, use_global_creds=ARGS.use_global_creds).create_payload()
+        print(HostDiscoveryWrapper(base_url=base_url, omeIp=ARGS.ip, vcUsercredential=credential, vCenterUUID=ARGS.vcUUID, payload=payload, jobname=ARGS.jobname, jobdescription=ARGS.jobdescription, console_entity_id=ARGS.console_entity_id, device_username=ARGS.device_username, device_password=ARGS.device_password, use_global_creds=ARGS.use_global_creds).run_discovery())
     else:
         print("Required parameters missing. Please review module help.")
