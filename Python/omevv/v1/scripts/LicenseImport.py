45914ca531b86721b8c38f0e437f88b95d6d3905
R8,Ramya <Ramya_R8@Dell.com>
2022-12-16 15:49:17 -0800

    Issue3:Added OMEA License Import TC_review comments addressed

--- Python/omevv/v1/scripts/LicenseImport.py
+++ Python/omevv/v1/scripts/LicenseImport.py
@@ -1,8 +1,6 @@
 #
 # LicenseImport. Python script using Redfish API with OEM extension to perform import of iDRAC OMEA license(s).
 #
-#
-#
 # _author_ = Ramya.R <Ramya.R@Dell.com>
 # _version_ = 1.0
 #
@@ -21,7 +19,10 @@ Description: LicenseImport. Python script using Redfish API with OEM extension t
 script_examples:
 
 1. this example will import iDRAC OMEA license from NFS share
-    python LicenseImport.py -ip 192.168.0.120 -u root -p calvin -ipaddress 192.168.0.130 -sharetype NFS -sharename /nfs -username <share username> -password <share password> -licensefilename iDRAC_OMEA_license.xml --retry 2')
+    python LicenseImport.py -ip 192.168.0.120 -u root -p calvin -ipaddress 192.168.0.130 -sharetype NFS -sharename /nfs -username <share username> -password <share password> -licensefilename iDRAC_OMEA_license.xml -retry 2
+
+2. To view the help menu for list of args to be passed:
+    python LicenseImport.py -h
 
 '''
 
@@ -30,170 +31,182 @@ import requests, json, sys, re, time, os, warnings, argparse
 from datetime import datetime
 
 warnings.filterwarnings("ignore")
-
-parser = argparse.ArgumentParser(
-    description="Python script using Redfish API with OEM extension to import iDRAC OMEA license(s)")
-parser.add_argument('-ip', help='iDRAC IP address', required=True)
-parser.add_argument('-u', help='iDRAC username', required=True)
-parser.add_argument('-p', help='iDRAC password', required=True)
-parser.add_argument('-sharetype',
-                    help='Pass in the share type of the network share to import iDRAC license,supported values:CIFS/NFS/HTTP/HTTPS',
-                    required=True)
-parser.add_argument('-ipaddress', help='Pass in the IP address of the network share to import iDRAC license',
-                    required=True)
-parser.add_argument('-sharename', help='Pass in the network share name for export / import iDRAC license',
-                    required=True)
-parser.add_argument('-username', help='Pass in the network share username',
-                    required=True)
-parser.add_argument('-password', help='Pass in the network share username password',
-                    required=True)
-parser.add_argument('--workgroup', help='Pass in the workgroup of your CIFS network share. This argument is optional',
-                    required=False)
-parser.add_argument('--ignorecertwarning',
-                    help='Supported values are On and Off. This argument is only required if using HTTPS for share type',
-                    required=False)
-parser.add_argument('-licensefilename', help='Pass in name of the license file on the network share you want to import',
-                    required=True)
-parser.add_argument('--retry',
-                    help='Pass in the number of times the command needs to be re-tried in case of exception,default value is 2',
-                    required=False)
-args = vars(parser.parse_args())
-
-idrac_ip = args["ip"]
-idrac_username = args["u"]
-idrac_password = args["p"]
-job_id = ""
-licenseimporturl = ""
-retry = 2
-payload = {"FQDD": "iDRAC.Embedded.1", "ImportOptions": "Force"}
+import base64
 
 
-def get_userinput():
-    global retry
-    if args["licensefilename"]:
-        payload["LicenseName"] = args["licensefilename"]
-    if args["ipaddress"]:
-        payload["IPAddress"] = args["ipaddress"]
-    if args["sharetype"]:
-        payload["ShareType"] = args["sharetype"]
-    if args["sharename"]:
-        payload["ShareName"] = args["sharename"]
-    if args["username"]:
-        payload["UserName"] = args["username"]
-    if args["password"]:
-        payload["Password"] = args["password"]
-    if args["workgroup"]:
-        payload["Workgroup"] = args["workgroup"]
-    if args["ignorecertwarning"]:
-        payload["IgnoreCertificateWarning"] = args["ignorecertwarning"]
-    if args["retry"]:
-        retry = args["retry"]
+class LicenseImport:
+    def __init__(self, idrac_ip, idrac_uname, idrac_pwd):
+        self.idrac_ip = idrac_ip
+        self.idrac_uname = idrac_uname
+        self.idrac_pwd = idrac_pwd
 
-    print("\n- WARNING, arguments and values for License Import method\n")
-    for i in payload.items():
-        if i[0] == "ShareParameters":
-            for ii in i[1].items():
-                if ii[0] == "Password":
-                    print("Password: **********")
-                else:
-                    print("%s: %s" % (ii[0], ii[1]))
-        else:
-            print("%s: %s" % (i[0], i[1]))
+        self.headers = {'content-type': 'application/json'}
+        credential = idrac_uname + ":" + idrac_pwd
+        self.encoded_idrac_cred = "Basic %s" % base64.b64encode(credential.encode('utf-8')).decode()
+        self.headers["Authorization"] = self.encoded_idrac_cred
 
+        self.job_id = ""
+        self.licenseimporturl = "/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellLicenseManagementService/Actions/DellLicenseManagementService.ImportLicenseFromNetworkShare"
+        self.config_url = "https://" + self.idrac_ip
+        self.retry = 2
 
-def import_idrac_license_network_share():
-    global retry, job_id
-    try:
-        headers = {'content-type': 'application/json'}
-        method = "ImportLicenseFromNetworkShare"
-        url = 'https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLicenseManagementService/Actions/DellLicenseManagementService.ImportLicenseFromNetworkShare' % (
-            idrac_ip)
+        self.payload = {}
 
-        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=False,
-                                 auth=(idrac_username, idrac_password))
-        data = response.json()
-        status_code = response.status_code
-        if response.status_code == 202:
-            print("\n- PASS: POST command passed for %s method, status code %s returned\n" % (
-            method, response.status_code))
-        # 500 series status codes
-        elif response.status_code >= 500:
-            print("\n- FAIL:POST command failed for %s method, status code %s.Retry after sometime\n" % (
-            method, status_code))
-            print("\n- POST command failure results:\n %s" % data)
-            sys.exit()
-        # 400 series status codes
-        else:
-            print("\n- FAIL: POST command failed for %s method, status code %s\n" % (method, status_code))
-            print("\n- POST command failure results:\n %s" % data)
-            sys.exit()
+    def create_payload(self, licensename, shareip, sharetype, sharename, shareuname=None, sharepwd=None, workgroup=None,
+                       ignorecertwarning=False):
+        self.payload = {"FQDD": "iDRAC.Embedded.1", "ImportOptions": "Force", "UserName": shareuname,
+                        "ShareType": sharetype,
+                        "ShareName": sharename, "Password": sharepwd, "IPAddress": shareip, "LicenseName": licensename,
+                        "Workgroup": workgroup, "IgnoreCertWarning": ignorecertwarning,
+                        }
+        self.payload = {k: v for k, v in self.payload.items() if v}
 
-        job_id = response.headers.get('Location')
-        if not (job_id):
-            print("- FAIL, unable to find job ID in headers POST response, headers output is:\n%s" % response.headers)
-            sys.exit()
-        print("- PASS, job ID %s successfuly created for %s method\n" % (job_id, method))
+        print("\n Payload is: {}".format(self.payload))
+        return self.payload
 
-    except Exception as e:
-        print("\n- FAIL: Exception %s occured" % e)
-        if retry > 0:
-            retry = retry - 1
-            time.sleep(5)
-            import_idrac_license_network_share()
-        else:
-            print("- FAIL: Exception %s occured even after retrying %s times" % (e, retry))
-            sys.exit()
-
-
-def loop_job_status():
-    start_time = datetime.now()
-    time.sleep(1)
-    while True:
-        req = requests.get('https://%s%s' % (idrac_ip, job_id),
-                           auth=(idrac_username, idrac_password), verify=False)
-        current_time = (datetime.now() - start_time)
-        statusCode = req.status_code
-        print("Statuscode", statusCode)
-        data = req.json()
-        if statusCode == 200:
-            pass
-        else:
-            print("\n- FAIL, Command failed to check job status, return code is %s" % statusCode)
-            print("Extended Info Message: {0}".format(req.json()))
-            sys.exit()
-        print(req.text)
-
-        if str(current_time)[0:7] >= "0:05:00":
-            print("\n- FAIL: Timeout of 5 minutes has been hit, script stopped\n")
-            sys.exit()
-        elif "Fail" in data[u'Message'] or "fail" in data[u'Message'] or data[u'JobState'] == "Failed":
-            print("\n- FAIL: job ID %s failed, failed message is: %s" % (job_id, data[u'Message']))
-            sys.exit()
-        elif data[u'JobState'] == "Completed":
-            if data[u'Message'] == "The command was successful":
-                print("\n--- PASS, Final Detailed Job Status Results ---\n")
-            else:
-                print("\n--- FAIL, Final Detailed Job Status Results ---\n")
-            for i in data.items():
-                if "odata" in i[0] or "MessageArgs" in i[0] or "TargetSettingsURI" in i[0]:
+    def check_job_status(self, job_id, retry=2):
+        flag = False
+        self.retry = retry
+        start_time = datetime.now()
+        try:
+            while True:
+                req = requests.get(self.config_url + job_id,
+                                   headers=self.headers, verify=False)
+                current_time = (datetime.now() - start_time)
+                statusCode = req.status_code
+                print("Statuscode", statusCode)
+                data = req.json()
+                if statusCode == 200:
                     pass
                 else:
-                    print("%s: %s" % (i[0], i[1]))
-            break
-        else:
-            print("- WARNING, JobStatus not completed, current job status execution time is: \"%s\"" % (
-                str(current_time)[0:7]))
+                    print("\n- FAIL, Command failed to check job status, return code is %s" % statusCode)
+                    print("Extended Info Message: {0}".format(req.json()))
+                    return flag
+                print(req.text)
+
+                if str(current_time)[0:7] >= "0:05:00":
+                    print("\n- FAIL: Timeout of 5 minutes has been hit, script stopped\n")
+                    return flag
+                elif "Fail" in data[u'Message'] or "fail" in data[u'Message'] or data[u'JobState'] == "Failed":
+                    print("\n- FAIL: job ID %s failed, failed message is: %s" % (job_id, data[u'Message']))
+                    return flag
+                elif data[u'JobState'] == "Completed":
+                    if data[u'Message'] == "The command was successful":
+                        print("\n--- PASS, Final Detailed Job Status Results ---\n")
+                        flag = True
+                    else:
+                        print("\n--- FAIL, Final Detailed Job Status Results ---\n")
+                    for i in data.items():
+                        if "odata" in i[0] or "MessageArgs" in i[0] or "TargetSettingsURI" in i[0]:
+                            pass
+                        else:
+                            print("%s: %s" % (i[0], i[1]))
+                    break
+                else:
+                    print("- WARNING, JobStatus not completed, current job status execution time is: \"%s\"" % (
+                        str(current_time)[0:7]))
+        except Exception as e:
+            print("\n- FAIL: Exception %s occured" % e)
+            if retry > 0:
+                retry = retry - 1
+                time.sleep(5)
+                self.check_job_status(job_id=self.job_id, retry=retry)
+
+        return flag
+
+    def import_idrac_license_network_share(self, payload, retry=2):
+        self.retry = retry
+        try:
+            method = "ImportLicenseFromNetworkShare"
+
+            print("- Performing POST on %s " % (self.config_url + self.licenseimporturl))
+            response = requests.post(self.config_url + self.licenseimporturl, data=json.dumps(payload),
+                                     headers=self.headers,
+                                     verify=False)
+            data = response.json()
+            status_code = response.status_code
+            if response.status_code == 202:
+                print("\n- PASS: POST command passed for %s method, status code %s returned\n" % (
+                    method, response.status_code))
+            # 500 series status codes
+            elif response.status_code >= 500:
+                print("\n- FAIL:POST command failed for %s method, status code %s.Retry after sometime\n" % (
+                    method, status_code))
+                print("\n- POST command failure results:\n %s" % data)
+
+            # 400 series status codes
+            else:
+                print("\n- FAIL: POST command failed for %s method, status code %s\n" % (method, status_code))
+                print("\n- POST command failure results:\n %s" % data)
+
+            self.job_id = response.headers.get('Location')
+            if not (self.job_id):
+                print(
+                    "- FAIL, unable to find job ID in headers POST response, headers output is:\n%s" % response.headers)
+                return ""
+            print("- PASS, job ID %s successfuly created for %s method\n" % (self.job_id, method))
+
+        except Exception as e:
+            print("\n- FAIL: Exception %s occured" % e)
+            if retry > 0:
+                retry = retry - 1
+                time.sleep(5)
+                self.import_idrac_license_network_share(payload, retry)
+            else:
+                print("- FAIL: Exception %s occured even after retrying %s times" % (e, self.retry))
+                return False
+
+        return self.job_id
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Python script using Redfish API with OEM extension to import iDRAC OMEA license(s)")
 
-    get_userinput()
-    import_idrac_license_network_share()
+    parser.add_argument('-ip', help='iDRAC IP address', required=True)
+    parser.add_argument('-u', help='iDRAC username. If no username is passed-default username of root is applied',
+                        required=False, default="root")
+    parser.add_argument('-p', help='iDRAC password', required=True)
+    parser.add_argument('-sharetype',
+                        help='Pass in the share type of the network share to import iDRAC license,supported values:CIFS/NFS/HTTP/HTTPS',
+                        required=True)
+    parser.add_argument('-ipaddress', help='Pass in the IP address of the network share to import iDRAC license',
+                        required=True)
+    parser.add_argument('-sharename', help='Pass in the network share name for export / import iDRAC license',
+                        required=True)
+    parser.add_argument('-username', help='Pass in the network share username',
+                        required=False)
+    parser.add_argument('-password', help='Pass in the network share username password',
+                        required=False)
+    parser.add_argument('-workgroup',
+                        help='Pass in the workgroup of your CIFS network share. This argument is optional',
+                        required=False)
+    parser.add_argument('-ignorecertwarning',
+                        help='Supported values are On and Off. This argument is only required if using HTTPS for share type',
+                        required=False)
+    parser.add_argument('-licensefilename',
+                        help='Pass in name of the license file on the network share you want to import',
+                        required=True)
+    parser.add_argument('-retry',
+                        help='Pass in the number of times the command needs to be re-tried in case of exception. The default value is retry=2',
+                        required=False, type=int)
+    args = vars(parser.parse_args())
+
+    licenseobj = LicenseImport(idrac_ip=args["ip"], idrac_uname=args["u"], idrac_pwd=args["p"])
+
+    payload = licenseobj.create_payload(licensename=args["licensefilename"], shareip=args["ipaddress"],
+                                        sharetype=args["sharetype"],
+                                        shareuname=args["username"], sharepwd=args["password"],
+                                        workgroup=args["workgroup"],
+                                        ignorecertwarning=args["ignorecertwarning"], sharename=args["sharename"])
+
+    job_id = licenseobj.import_idrac_license_network_share(payload=payload, retry=args["retry"])
 
     if job_id:
         # call job func only if Job id is returned
-        loop_job_status()
-
+        flag = licenseobj.check_job_status(job_id, retry=args["retry"])
+        if not (flag):
+            print("- FAIL: Job for license import is not successful")
 
 
 
