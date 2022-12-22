bfa283724f33eab67cd2e35e4aec61e084c67738
KristianLamb <115053982+KristianLamb@users.noreply.github.com>
2022-12-21 19:09:21 -0600

    Update autoManageCompliantHosts.py
    + Renamed functions so behavior is more clear
    + Additional Error handling

--- Python/omevv/v1/scripts/autoManageCompliantHosts.py
+++ Python/omevv/v1/scripts/autoManageCompliantHosts.py
@@ -1,18 +1,17 @@
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union, Tuple
 import argparse
 import sys
 import constants
 import time
 from omevvServerManagement import HostsManagementWrapper
 from omevv_apis_client.models import Credential
-
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 retry = 3
 
 
-def getHostCompliance(ip, vcusername, vcpassword, vcuuid):
+def getDiscoveredHosts(ip, vcusername, vcpassword, vcuuid) -> Tuple[bool,dict]:
     global retry
     headers = {constants.vcGuidHeader: vcuuid}
     headers["Content-Type"] = "application/json"
@@ -20,29 +19,28 @@ def getHostCompliance(ip, vcusername, vcpassword, vcuuid):
     try:
         response = requests.get(url, headers=headers, verify=False, auth=(vcusername,vcpassword))
         response.raise_for_status()
-        return response.json()
-    except Exception as e:
+        return (True, response.json())
+    except requests.ReadTimeout as e:
         print("Exception occured while querying available host compliance ",e," retrying ..")
         if retry > 0:
             retry -= 1
             time.sleep(5)
-            getHostCompliance(ip, vcusername, vcpassword, vcuuid)
+            getDiscoveredHosts(ip, vcusername, vcpassword, vcuuid)
         else:
-            print("Failed after 3 retries,exiting")
-            sys.exit()
+            return (False, {"Error": e})
+    except requests.HTTPError as e:
+        return (False, {"Error": e})
 
 
-def getCompliantHosts(discovered_hosts):
+def getCompliantHostIds(discovered_hosts) -> Tuple[bool,list]:
     compliant_hosts = []
     for i in discovered_hosts:
         if i["state"] == "COMPLIANT":
             compliant_hosts.append(i["hostid"])
     if compliant_hosts:
-        print(f"Found {len(compliant_hosts)} Compliant Discovered hosts to be managed")
-        return compliant_hosts
+        return (True, compliant_hosts)
     else:
-        print("No Compliant Hosts available to Manage")
-        sys.exit()
+        return (False, compliant_hosts)
 
 
 if __name__ == "__main__":
@@ -57,17 +55,21 @@ if __name__ == "__main__":
                         help="UUID of the relevant vCenter")
     ARGS = PARSER.parse_args()
 
-    discovered_hosts = getHostCompliance(ARGS.ip, ARGS.vcusername, ARGS.vcpassword, ARGS.vcUUID)
-    if not discovered_hosts:
-        print("No Discovered hosts waiting to be Managed")
-        sys.exit()
-
-    host_ids = getCompliantHosts(discovered_hosts)
-    base_url = 'https://{ip}/omevv/GatewayService/v1/'.format(ip=ARGS.ip)
-    credential = Credential(username=ARGS.vcusername, password=ARGS.vcpassword)
-    jobname = f"API ManageJob-{time.ctime()}"
-    hostmgmthelper = HostsManagementWrapper(base_url=base_url, omeIp=ARGS.ip, vcUsercredential=credential, \
-                                    vCenterUUID=ARGS.vcUUID, payload={}, jobname=jobname, \
-                                    jobdescription=None, host_ids=host_ids)
-    hostmgmthelper.create_payload()
-    print(hostmgmthelper.manage())
+    success,response = getDiscoveredHosts(ARGS.ip, ARGS.vcusername, ARGS.vcpassword, ARGS.vcUUID)
+    if success and response:
+        success,host_ids = getCompliantHostIds(response)
+        if success:
+            base_url = 'https://{ip}/omevv/GatewayService/v1/'.format(ip=ARGS.ip)
+            credential = Credential(username=ARGS.vcusername, password=ARGS.vcpassword)
+            jobname = f"API ManageJob-{time.ctime()}"
+            hostmgmthelper = HostsManagementWrapper(base_url=base_url, omeIp=ARGS.ip, vcUsercredential=credential, \
+                                            vCenterUUID=ARGS.vcUUID, payload={}, jobname=jobname, \
+                                            jobdescription=None, host_ids=host_ids)
+            hostmgmthelper.create_payload()
+            print(hostmgmthelper.manage())
+        else:
+            print("SUCESS: No Discovered Hosts matching COMPLIANT state.")
+    elif success and len(response) == 0:
+        print("SUCCESS: No Unmanaged Hosts")
+    else:
+        print("FAILED: Error %s" % response)
