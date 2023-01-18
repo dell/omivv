cf90b702609b10c920f6eb2da0da31ac29724c74
KristianLamb <115053982+KristianLamb@users.noreply.github.com>
2023-01-17 23:02:28 -0600

    Usr/kristianlamb/issue 32 auto manage all compliant servers (#34)
    * OMEVV Management of server devices
    
    * OMEVV Management of server devices
    
    * Updated directory structure
    
    * Clean
    
    * Create autoManageCompliantHosts.py
    
    * Update getManagedHosts.py
    
    * Create constants.py
    
    * Update autoManageCompliantHosts.py
    
    + Renamed functions so behavior is more clear
    + Additional Error handling
    
    * Update autoManageCompliantHosts.py
    
    Allow NONCOMPLIANT state to be managed
    
    * Issue3_Update OME Advanced+ license to a single idrac node  (#16)
    
    * Issue3:Added OMEA License Import TC
    
    * Issue3:Added OMEA License Import TC_review comments addressed
    
    * Issue3:Added changes to pass the indrac details as part of the function instead of the constructor- to make it easier for function call - when used as a module
    
    Co-authored-by: R8,Ramya <Ramya_R8@Dell.com>
    
    * Discover server device (#27)
    
    * Committing discovery script
    
    * Discover Server Device
    
    * Discover Server Device - Updates
    
    * Discover Server Device - Updates
    
    * Discover Server Device - Updates
    
    * Discover Server Device - Updates
    
    * Discover Server Device - Updates
    
    * Discover Server Device - Updates
    
    * Discover Server Device - Updates
    
    * Updated directory structure
    
    * Clean
    
    * Clean
    
    * Clean
    
    * Clean
    
    * Updates
    
    * Updates
    
    * OMEVV Management of server devices (#28)
    
    * OMEVV Management of server devices
    
    * OMEVV Management of server devices
    
    * Updated directory structure
    
    * Clean
    
    * Clean
    
    * Clean
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Removing server from OMEVV Management (#29)
    
    * Removing server from OMEVV Management
    
    * Removing server from OMEVV Management - Updates
    
    * Updated directory structure
    
    * Clean
    
    * Clean
    
    * Clean
    
    * Updates
    
    * vCenter(s) unregistration (#30)
    
    * vCenter(s) unregistration
    
    * vCenter(s) unregistration - Updates
    
    * Updated directory structure
    
    * Clean
    
    * Clean
    
    * Clean
    
    * Updates
    
    * Updates
    
    * String update
    
    * Modify drift detection schedule for a vCenter cluster in OMEVVP (#33)
    
    * Modify drift detection schedule for a vCenter cluster in OMEVVP
    
    * Updated directory structure
    
    * Clean
    
    * Clean
    
    * Clean
    
    * Clean
    
    * Clean
    
    * Updates
    
    * Updates
    
    * OMEVV Management of server devices
    
    * OMEVV Management of server devices
    
    * Updated directory structure
    
    * Create autoManageCompliantHosts.py
    
    * Update autoManageCompliantHosts.py
    
    + Renamed functions so behavior is more clear
    + Additional Error handling
    
    * Update autoManageCompliantHosts.py
    
    Allow NONCOMPLIANT state to be managed
    
    * Clean
    
    * Clean
    
    * Clean
    
    Co-authored-by: Abhishek_Gupta14 <Abhishek_Gupta14@dell.com>
    Co-authored-by: pavithra-mahadev <117452247+pavithra-mahadev@users.noreply.github.com>
    Co-authored-by: ramya-dell <114721297+ramya-dell@users.noreply.github.com>
    Co-authored-by: R8,Ramya <Ramya_R8@Dell.com>
    Co-authored-by: Abhishek14-Dell <114723348+Abhishek14-Dell@users.noreply.github.com>

---
+++ Python/omevv/v1/scripts/autoManageCompliantHosts.py
@@ -0,0 +1,75 @@
+from typing import Any, Dict, List, Optional, Union, Tuple
+import argparse
+import sys
+import constants
+import time
+from omevvServerManagement import HostsManagementWrapper
+from omevv_apis_client.models import Credential
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+retry = 3
+
+
+def getDiscoveredHosts(ip, vcusername, vcpassword, vcuuid) -> Tuple[bool,dict]:
+    global retry
+    headers = {constants.vcGuidHeader: vcuuid}
+    headers["Content-Type"] = "application/json"
+    url = f"https://{ip}/omevv/GatewayService/v1/Consoles/{vcuuid}/Compliance"
+    try:
+        response = requests.get(url, headers=headers, verify=False, auth=(vcusername,vcpassword))
+        response.raise_for_status()
+        return (True, response.json())
+    except requests.ReadTimeout as e:
+        print("Exception occured while querying available host compliance ",e," retrying ..")
+        if retry > 0:
+            retry -= 1
+            time.sleep(5)
+            getDiscoveredHosts(ip, vcusername, vcpassword, vcuuid)
+        else:
+            return (False, {"Error": e})
+    except requests.HTTPError as e:
+        return (False, {"Error": e})
+
+
+def getValidHostIds(discovered_hosts) -> Tuple[bool,list]:
+    valid_hosts = []
+    for i in discovered_hosts:
+        if i["state"] in ["COMPLIANT","NONCOMPLIANT"]:
+            valid_hosts.append(i["hostid"])
+    if valid_hosts:
+        return (True, valid_hosts)
+    else:
+        return (False, valid_hosts)
+
+
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    PARSER.add_argument("--ip", "-i", required=True, help="OME Appliance IP")
+    PARSER.add_argument("--vcusername", "-u", required=True,
+                        help="username of vcenter")
+    PARSER.add_argument("--vcpassword", "-p", required=True,
+                        help="password of vcenter")
+    PARSER.add_argument("--vcUUID", "-d", required=True, default=None,
+                        help="UUID of the relevant vCenter")
+    ARGS = PARSER.parse_args()
+
+    success,response = getDiscoveredHosts(ARGS.ip, ARGS.vcusername, ARGS.vcpassword, ARGS.vcUUID)
+    if success and response:
+        success,host_ids = getValidHostIds(response)
+        if success:
+            base_url = 'https://{ip}/omevv/GatewayService/v1/'.format(ip=ARGS.ip)
+            credential = Credential(username=ARGS.vcusername, password=ARGS.vcpassword)
+            jobname = f"API ManageJob-{time.ctime()}"
+            hostmgmthelper = HostsManagementWrapper(base_url=base_url, omeIp=ARGS.ip, vcUsercredential=credential, \
+                                            vCenterUUID=ARGS.vcUUID, payload={}, jobname=jobname, \
+                                            jobdescription=None, host_ids=host_ids)
+            hostmgmthelper.create_payload()
+            print(hostmgmthelper.manage())
+        else:
+            print("SUCESS: No Discovered Hosts available to be Managed. Please see Management Compliance State for Reason.")
+    elif success and len(response) == 0:
+        print("SUCCESS: No Unmanaged Hosts")
+    else:
+        print("FAILED: Error %s" % response)
