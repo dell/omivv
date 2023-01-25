b78200698d9a5a06350e2dd4d0ffe039ec15549c
Prasanna <prasanna_j@dell.com>
2023-01-25 15:59:29 +0530

    Re-arranged some the functions. Add status code is successfully few API call which i'm making. Printing the progress of the script.

--- Python/omevv/v1/scripts/vSphereHostFirmwareUpdate.py
+++ Python/omevv/v1/scripts/vSphereHostFirmwareUpdate.py
@@ -33,6 +33,44 @@ class FWUpdateWrapper:
             with_headers(headers=headers). \
             with_timeout(constants.generalTimeOut_sec)
 
+    def check_response_code_and_return(self, response, message):
+        if response.status_code == 200:
+            response_dict = json.loads(response.content.decode("utf-8").replace("'", '"'))
+            return response_dict, response.status_code
+        else:
+            print("ERROR: " + message)
+            print("Error code: ", response["errorCode"])
+            print("Error Message: ", response["message"])
+            exit(1)
+
+    def get_managed_host_list(self):
+        response: Response[Union[ErrorObject, List[ManagedHost]]] = get_managed_hosts.sync_detailed(uuid=self.uuid,
+                                                                                                    client=self.client)
+        response_dict, response.status_code = self.check_response_code_and_return(response,
+                                                                                  message="Unable to get the managed host list")
+        return response_dict, response.status_code
+
+    def get_group_detail(self, omevv_group_id):
+        response: Response[Union[ErrorObject, Group]] = get_group_by_id.sync_detailed(uuid=self.uuid,
+                                                                                      client=self.client,
+                                                                                      gid=omevv_group_id)
+        response_dict, response.status_code = self.check_response_code_and_return(response,
+                                                                                  message="Failed to get group detail")
+        return response_dict, response.status_code
+
+    def get_cluster_detail(self, cluster_id):
+        response: Response[list[ConsoleEntity]] = get_console_clusters.sync_detailed(uuid=self.uuid, client=self.client,
+                                                                                     entityid=cluster_id)
+        response_dict, response.status_code = self.check_response_code_and_return(response,
+                                                                                  message="Failed to get cluster detail")
+        return response_dict, response.status_code
+
+    def can_fw_update(self, omevv_group_id):
+        response: Response[bool] = can_trigger_update.sync_detailed(uuid=self.uuid, client=self.client,
+                                                                    json_body=omevv_group_id)
+        response_dict = json.loads(response.content.decode("utf-8").replace("'", '"'))
+        return response_dict, response.status_code
+
     def get_firmware_drift_report_by_host_id(self, omevv_group_id, host_id):
         response: Response[Union[ErrorObject, FirmwareDriftResponseModel]] = get_firmware_drift_report_by_host_id. \
             sync_detailed(uuid=self.uuid, client=self.client, gid=omevv_group_id, hid=host_id)
@@ -92,32 +130,6 @@ class FWUpdateWrapper:
         response_dict = json.loads(response.content.decode("utf-8").replace("'", '"'))
         return response_dict, response.status_code
 
-    def get_managed_host_list(self):
-        response: Response[Union[ErrorObject, List[ManagedHost]]] = get_managed_hosts.sync_detailed(uuid=self.uuid,
-                                                                                                    client=self.client)
-        response_dict = json.loads(response.content.decode("utf-8").replace("'", '"'))
-        return response_dict, response.status_code
-
-    def can_fw_update(self, omevv_group_id):
-        response: Response[bool] = can_trigger_update.sync_detailed(uuid=self.uuid, client=self.client,
-                                                                    json_body=omevv_group_id)
-        response_dict = json.loads(response.content.decode("utf-8").replace("'", '"'))
-        return response_dict, response.status_code
-
-    def get_group_detail(self, omevv_group_id):
-        response: Response[Union[ErrorObject, Group]] = get_group_by_id.sync_detailed(uuid=self.uuid,
-                                                                                      client=self.client,
-                                                                                      gid=omevv_group_id)
-        response_dict = json.loads(response.content.decode("utf-8").replace("'", '"'))
-        return response_dict, response.status_code
-
-    def get_cluster_detail(self, cluster_id):
-        response: Response[list[ConsoleEntity]] = get_console_clusters.sync_detailed(uuid=self.uuid,
-                                                                                     client=self.client,
-                                                                                     entityid=cluster_id)
-        response_dict = json.loads(response.content.decode("utf-8").replace("'", '"'))
-        return response_dict, response.status_code
-
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(description=__doc__,
@@ -164,6 +176,7 @@ if __name__ == "__main__":
         if not any(d1.get('id') == ARGS.host_id for d1 in response_content):
             print("ERROR: Provided host_id(" + str(ARGS.host_id) + ") is not managed by OMEVV")
             exit(1)
+        print("Host is managed by OMEVV")
         omevv_group_id = next(element for element in response_content if element['id'] == ARGS.host_id)["omevvpGroupId"]
 
         # Get groupType and consoleEntityId
@@ -172,7 +185,8 @@ if __name__ == "__main__":
         groupType = response_content["groupType"]
         # Checking host is from the cluster group. Fail if the datacenter host is given
         if groupType != "CLUSTER":
-            print("ERROR: Provided host_id(" + str(ARGS.host_id) + ") does not belongs to a cluster. Host group type is " + str(groupType))
+            print("ERROR: Provided host_id(" + str(ARGS.host_id) + ") does not belongs to a cluster. Host group type "
+                                                                   "is " + str(groupType))
             exit(1)
         consoleEntityId = response_content["consoleEntityId"]
         # Get vSANEnabled status
@@ -182,6 +196,7 @@ if __name__ == "__main__":
         if response_content[0]["vsanEnabled"]:
             print("ERROR: Provided host_id(" + str(ARGS.host_id) + ") does not belongs to vSphere cluster")
             exit(1)
+        print("Host is a vSphere host")
 
         # checking no Firmware update is running for the host
         response_content, status_code = FWUpdateWrapper(base_url=base_url, vcUsercredential=credential,
@@ -206,6 +221,8 @@ if __name__ == "__main__":
                 exclude_components = ["EQUAL", "UNKNOWN"]
                 final_components = [d for d in all_components if d['updateAction'] not in exclude_components]
                 sourceName_list = [sub['sourceName'] for sub in final_components]
+                print("Following component(s) going to be updated:")
+                print(*[sub['componentName'] for sub in final_components], sep="\n")
         else:
             print("ERROR: Unable to get the firmware drift for the host.")
             print("Error code: ", host_compliance["errorCode"])
@@ -238,6 +255,8 @@ if __name__ == "__main__":
             job_detail, status_code = FWUpdateWrapper(base_url=base_url, vcUsercredential=credential,
                                                       vCenterUUID=ARGS.vcUUID). \
                 firmware_update_job_by_id(job_id)
+            print("Firmware update job has been triggered. Job name is: " + job_detail['jobName'])
+            print("Waiting for the job to be completed...")
             if status_code == 200:
                 while job_detail['state'] == "RUNNING":
                     time.sleep(30)
