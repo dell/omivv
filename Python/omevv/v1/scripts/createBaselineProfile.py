112c9e4c6ce9b1693bb9b91f664beb860402145b
R8,Ramya <Ramya_R8@Dell.com>
2023-01-31 12:38:28 -0800

    Issue7:Added changes to CreateBaselineprofile to include days of week as args seperately and minor changes to License import script

--- Python/omevv/v1/scripts/createBaselineProfile.py
+++ Python/omevv/v1/scripts/createBaselineProfile.py
@@ -1,22 +1,24 @@
 #
-# Baseline profile:Creating a Baseline profile in OMEVVP
-# Baseline Profile enables user to capture the baseline configuration for system configuration, firmware, or driver versions
-# and maintain the desired state for clusters by identifying the drift against the baseline.
-#
-#
+# LicenseImport. Python script for Creating a Baseline profile in OMEVVP
 #
 # _author_ = Ramya.R <Ramya.R@Dell.com>
 # _version_ = 1.0
 #
-# Copyright (c) 2022, Dell, Inc.
+# Copyright (c) 2023 Dell EMC Corporation
 #
-# This software is licensed to you under the GNU General Public License,
-# version 2 (GPLv2). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
-# along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 
 '''
 Description: BaselineProfile. Creating a Baseline profile in OMEVVP
@@ -24,7 +26,7 @@ Description: BaselineProfile. Creating a Baseline profile in OMEVVP
 
 script_examples:
 1. this example will CreateBaselineProfile with job schedule for monday,tuesday at time 14:15
-python createBaselineProfile.py -ip 192.168.0.120 -vcuser <vc username> -name <baseline profile name> -desc ExampleBaselineProfile -vcpass <vc password> -vcuuid <vcuuid> -frid <Firmware repoid> -gid <groupid> -jsd monday,tuesday -jst 14:15 -retry 4
+python createBaselineProfile.py -ip 192.168.0.120 -vcuser <vc username> -name <baseline profile name> -desc ExampleBaselineProfile -vcpass <vc password> -vcuuid <vcuuid> -frid <Firmware repoid> -gid <groupid> -jst 14:15 -mon true -tue true -retry 4
 
 2. this example will CreateBaselineProfile with default job schedule sunday at time 03:00
 python createBaselineProfile.py -ip 192.168.0.120 -vcuser <vc username> -name <baseline profile name> -vcpass <vc password> -vcuuid <vcuuid> -frid <Firmware repoid> -gid <groupid>
@@ -56,54 +58,26 @@ class CreateBaselineProfile:
 
         self.headers = {'content-type': 'application/json'}
         self.baseline_profileid = ""
-        self.payload = {}
 
 
     #<--- Function to get user inputs--->
-    def create_payload(self,name,description,firmware_repoid,driver_repoid,time,retry,configuration_repoid,createdby,jobschedule_days,group_idslist=[]):
+    def create_payload(self,name,description,firmware_repoid,driver_repoid,time,retry,configuration_repoid,createdby,monday,tuesday,wednesday,thursday,friday,saturday,sunday,group_idslist=[]):
         self.retry = retry
+        self.monday = monday
+        self.tuesday = tuesday
+        self.wednesday = wednesday
+        self.thursday = thursday
+        self.friday = friday
+        self.saturday = saturday
+        self.sunday = sunday
 
-        self.payload = {
-            "name": name,
-            "description": description,
-            "firmwareRepoId": firmware_repoid,
-            "driverRepoId": driver_repoid,
-            "configurationRepoId": configuration_repoid,
-            "createdBy": createdby,
-            "groupIds": group_idslist,
-            "jobSchedule": {
-                "monday": False,
-                "tuesday": False,
-                "wednesday": False,
-                "thursday": False,
-                "friday": False,
-                "saturday": False,
-                "sunday": False,
-                "time": time
-            }
-        }
-        print(jobschedule_days)
-        #jobscheduledays
-        if jobschedule_days:
-            #When multiple days are mentioned
-            if "," in jobschedule_days:
-                split_string = jobschedule_days.split(",")
-                for day in split_string:
-                    if day in self.payload["jobSchedule"]:
-                        self.payload["jobSchedule"][day]= True
-                    else:
-                        print("\n FAIL, either missing argument or incorrect argument used. Please check script help-text for jsd.")
-                        print("\n Proceeding with default value for jobschedule-day")
-            else:
-                if (self.payload["jobSchedule"].get(jobschedule_days,None)) !=None:
-                    self.payload["jobSchedule"][jobschedule_days]= True
-                else:
-                    print("\n INFO, incorrect argument value for jobschedule-day. Please check script help-text for jsd.")
-                    print("\n Proceeding with default value for jobschedule-day")
+        #setting the default value- sunday to true if no job schedule days are passed
+        if not(self.monday or self.tuesday or self.wednesday or self.thursday or self.friday or self.saturday or self.sunday):
+            print("\n INFO, incorrect argument value for jobschedule-day. Proceeding with the default value jobschedule-day as sunday.")
+            self.sunday=True
 
 
-
-        self.day_of_week_obj=DaysAndTimeOfWeek(monday=self.payload["jobSchedule"]["monday"],tuesday=self.payload["jobSchedule"]["tuesday"],wednesday=self.payload["jobSchedule"]["wednesday"],thursday=self.payload["jobSchedule"]["thursday"],friday=self.payload["jobSchedule"]["friday"],saturday=self.payload["jobSchedule"]["saturday"],sunday=self.payload["jobSchedule"]["sunday"],time=self.payload["jobSchedule"]["time"])
+        self.day_of_week_obj=DaysAndTimeOfWeek(monday=self.monday,tuesday=self.tuesday,wednesday=self.wednesday,thursday=self.thursday,friday=self.friday,saturday=self.saturday,sunday=self.sunday,time=time)
 
         self.create_baseline_payload_obj = BaselineProfileCreateRequest(name=name,description=description,firmware_repo_id=firmware_repoid,driver_repo_id=driver_repoid,configuration_repo_id=configuration_repoid,created_by=createdby,group_ids=group_idslist,job_schedule=self.day_of_week_obj)
         print("\n payload attributes - ", self.create_baseline_payload_obj)
@@ -112,7 +86,7 @@ class CreateBaselineProfile:
 
 
     #<--- Function to  create_baselineprofile--->
-    def create_baselineprofile(self,payload,vc_username,vc_pwd,omevv_ip,vc_uuid,retry):
+    def create_baselineprofile(self,vc_username,vc_pwd,omevv_ip,vc_uuid,retry):
 
         self.vc_username=vc_username
         self.vc_pwd=vc_pwd
@@ -165,11 +139,11 @@ class CreateBaselineProfile:
 
         except Exception as e:
 
-            print("\n- FAIL: Exception %s occured" % e)
+            print("\n- FAIL: Exception occured- %s " % e)
             if retry > 0:
                 retry = retry - 1
                 time.sleep(5)
-                self.create_baselineprofile(self.payload,self.vc_username,self.vc_pwd,self.omevv_ip,self.vc_uuid,retry=retry)
+                self.create_baselineprofile(self.vc_username,self.vc_pwd,self.omevv_ip,self.vc_uuid,retry=retry)
             else:
                 print("- FAIL: Exception %s occured even after retrying %s times" % (e, self.retry))
                 return ""
@@ -200,32 +174,36 @@ if __name__ == "__main__":
 
     parser.add_argument('-gid', help="Pass in the groupId's e.g 1111", required=True,nargs='+')
 
-    parser.add_argument("-jsd", help="job schedule - Enable days of the week for baseline profile job schedule."
-                                      "Supported values are: monday,tuesday,wednesday,thursday,friday,saturday,sunday"
-                                       "If you pass in multiple string values, make sure to use comma separator Default:sunday",
-                        required=False,default="sunday")
-
     parser.add_argument("-jst", required=False,
                         help="Specify time for baseline profile job schedule-in 24 HR format: HH:SS. default value - 03:00",default="03:00")
 
     parser.add_argument('-retry',
-                        help='Pass in the number of times the command needs to be re-tried in case of exception,default value is 2',
-                        required=False,default=2)
+                        help='Pass in the number of times the command needs to be re-tried in case of exception,default value is 3',
+                        required=False,default=3)
+
+    parser.add_argument("-mon", required=False, default=False, help="indicate the boolean value for monday")
+    parser.add_argument("-tue", required=False, default=False,help="indicate the boolean value for tuesday")
+    parser.add_argument("-wed", required=False, default=False,help="indicate the boolean value for wednesday")
+    parser.add_argument("-thu", required=False, default=False, help="indicate the boolean value for thursday")
+    parser.add_argument("-fri", required=False, default=False, help="indicate the boolean value for friday")
+    parser.add_argument("-sat", required=False, default=False,help="indicate the boolean value for saturday")
+    parser.add_argument("-sun", required=False, default=False, help="indicate the boolean value for sunday")
 
     args = vars(parser.parse_args())
     print(args)
 
     baseline_obj=CreateBaselineProfile()
-    payload=baseline_obj.create_payload(name=args["name"],description=args["desc"],firmware_repoid=args["frid"],driver_repoid=args["drid"],
-                                                configuration_repoid=args["crid"],createdby=args["cb"],group_idslist=args["gid"],
-                                                jobschedule_days=args["jsd"],time=args["jst"],retry=args["retry"])
+    baseline_obj.create_payload(name=args["name"],description=args["desc"],firmware_repoid=args["frid"],driver_repoid=args["drid"],
+                                                configuration_repoid=args["crid"],createdby=args["cb"],group_idslist=args["gid"],time=args["jst"],retry=args["retry"],monday=args["mon"],tuesday=args["tue"],wednesday=args["wed"],thursday=args["thu"],friday=args["fri"],saturday=args["sat"],sunday=args["sun"])
 
 
-    baselineprofile_id=baseline_obj.create_baselineprofile(payload=payload,vc_username=args["vcuser"],vc_pwd=args["vcpass"],
+    baselineprofile_id=baseline_obj.create_baselineprofile(vc_username=args["vcuser"],vc_pwd=args["vcpass"],
                                                                  omevv_ip=args["ip"],vc_uuid=args["vcuuid"],retry=args["retry"])
 
     if not(baselineprofile_id):
         print("- FAIL:The Creation of Baseline Profile is not successful")
+
+
 
 
 
